d3d6edb62094b3115fb4bdb5ce2b6d006c0bd347
Foysal Ahamed <cfaion341@gmail.com>
2015-05-31 20:23:24 +0200

    fixed absolute uri parsing and added output to file with lots of cleanup

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
-var _ = require('lodash');
+var _ = require('lodash'),
+    fs = require('fs'),
+    helpers = require('./helpers');
 
 var casper = require("casper").create({
     pageSettings: {
-        loadImages: false,
+        loadImages: true,
         loadPlugins: false
+    },
+    viewportSize: {
+        width: 1600,
+        height: 950
     },
     // verbose: true,
     // logLevel: 'debug'
 });
 var checked = [];
 var currentLink = 0;
-var fs = require('fs');
-var upTo = ~~casper.cli.get('max-depth') || 100;
+var upTo = ~~casper.cli.get('max-depth') || 1000;
 var url = casper.cli.get(0);
 var baseUrl = url;
 var links = [url];
@@ -20,21 +25,13 @@ var sitemap = {};
 // filter out the links that are already checked or does not belong in the same domain
 function urlsToScrape(urls) {
 	// console.log('total links: ', urls.length);
-    var cleanUrls = _.filter(_.unique(urls), function(url) {
-        var hasHttp = url.indexOf('http') === 0,
-            isClean = false;
+    var cleanUrls = []
 
-        if (hasHttp) {
-            isCLean = url.indexOf(baseUrl) === 0;
-        } else {
-            isCLean = url.indexOf('/') === 0;
+    _.each(_.unique(urls), function(url) {
+        var parsedUrl = helpers.absoluteUri(baseUrl, url);
+        if (!_.contains(checked, parsedUrl) && parsedUrl.indexOf(baseUrl) === 0) {
+            cleanUrls.push(parsedUrl);
         }
-        // console.log('clean', isCLean);
-        return isCLean;
-    });
-
-    cleanUrls = _.filter(cleanUrls, function(url) {
-        return checked.indexOf(url) === -1;
     });
 
     // console.log('total clean urls', cleanUrls.length);
@@ -47,83 +44,93 @@ function crawl(url) {
         this.echo('visiting: '+ url, 'COMMENT');
         this.open(url);
     	checked.push(url);
-    	this.wait(2000);
     });
+    
+    // this.wait(10000);
 
     // this.then(function(res) {
-    //     sendImageToOctopus(this);
+        // helpers.sendImageToOctopus(this);
     // });
 
     this.then(function() {
-        var newUrls = getLinksOnPage.call(this),
-            filteredUrls = urlsToScrape(newUrls);
+        var newUrls = getLinksOnPage.call(this);
 
-        links = _.filter(links.concat(filteredUrls), function(url) {
-            return checked.indexOf(url) === -1;
-        });
-
+        // get new urls on the page that needs to be crawled
+        links = _.union(links, urlsToScrape(newUrls));
         // this.echo(filteredUrls.length + " new links found on " + url);
     });
 }
  
-// Fetch all <a> elements from the page and return the href attr from each
+// Fetch all <a> tags from the page and return the href attr from each
 function getLinksOnPage() {
-    function _fetchInternalLinks() {
-        var links = __utils__.findAll('a[href]').map(function(node) {
+    var links = this.evaluate(function () {
+        var hrefs = __utils__.findAll('a[href]').map(function(node) {
             return node.getAttribute('href');
         });
 
-        // console.log(links);
-        return links;
-    }
-    return this.evaluate(_fetchInternalLinks);
+        return hrefs;
+    });
+
+    return links;
 }
  
 // As long as it has a next link, and is under the maximum limit, will keep running
 function iteration() {
-    if (links[currentLink] && currentLink < upTo) {
-        sitemap[links[currentLink]] = [];
-        crawl.call(this, links[currentLink]);
-        currentLink++;
-        this.run(iteration);
-    } else {
-        this.echo("All done, " + checked.length + " links checked.");
-        
-        _.each(sitemap, function (resources, url) {
-            casper.echo('Page:'+ url, 'INFO'    );
-            
-            casper.echo("Total Static Resources: "+ resources.length, 'PARAMETER');            
-            // _.each(resources, function (resource) {
-            //     console.log("\t\t", resource.url);
-            // }); 
-        });
-
-        this.exit();
+    // exit if we cross the max-depth level 
+    if (currentLink >= upTo) {
+        casper.echo('Maximum depth exceeded!', 'WARNING');
+        return casper.exit();
     }
+
+    // exit if there are no more links to be crawled
+    if (!links[currentLink]) {
+        casper.echo('All Linked Pages are crawled', 'INFO');
+        return casper.exit();
+    }
+
+    sitemap[links[currentLink]] = []; // an empty container for all the assets on page
+    crawl.call(this, links[currentLink]);
+    currentLink++;
+    this.run(iteration);
 }
 
-function sendImageToOctopus(casper){
-	console.log('sending image');
-	casper.evaluate( function(img){
-        __utils__.sendAJAX("http://localhost:8002/", 'POST', {
-        	'img' : img 
-        }, false);
-    }, {
-      	'img' : casper.captureBase64('png')
-    }); // evaluate
+function storeSiteMap() {
+    if (_.isEmpty(sitemap)) {
+        casper.echo('No sitemap has been created', 'WARNING');
+        return;
+    }
 
-} // show
+    var currentFile = require('system').args[3],
+        currentFilePath = fs.absolute(currentFile).split('/'),
+        currentFilePath = currentFilePath.splice(0, currentFilePath.length - 1).join("/");
+
+    var now = new Date(),
+        filename = currentFilePath +'/sitemaps/'+ now.getTime() +'.json';
+    // console.log(_.size(sitemap), filename, JSON.stringify(sitemap).length);
+
+    fs.write(filename, JSON.stringify(sitemap, null, 2));
+    casper.echo('total pages: '+ _.size(sitemap));
+    casper.echo('Sitemap stored in file: '+ filename, 'TRACE');
+}
  
 if (!url) {
     casper.warn('No url passed, aborting.').exit();
 }
 
 casper.on('resource.received', function(resource) {
-    // console.log(links[currentLink-1]);
-    if (resource.url.indexOf('data:') === 0)
-        return;
+    var currentPage = sitemap[links[currentLink-1]],
+        isDataUri = resource.url.indexOf('data:') === 0,
+        staticContentMatcher = new RegExp("javascript|css|image|video", "i"),
+        isStaticContent = staticContentMatcher.exec(resource.contentType),
+        alreadyIndexed = _.contains(currentPage, resource.url);
 
-    sitemap[links[currentLink-1]].push(resource);
+    if (!alreadyIndexed && isStaticContent && !isDataUri) {
+        currentPage.push(resource.url);
+    }
+});
+
+casper.on('exit', function () {
+    storeSiteMap();
 });
  
 casper.start().then(function() {
